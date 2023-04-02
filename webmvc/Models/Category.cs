86974e54b78c0310db15c5d3aea9b8297dea92ae
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-04-02 12:12:07 +0700

    Add project files.

---
+++ webmvc/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace webmvc.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [DisplayName("Your Name")]
+        public string Name { get; set; }
+
+    }
+
+}
